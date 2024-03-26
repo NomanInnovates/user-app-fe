7c9a12ab65bb2733710da69766861520d23f5e3f
Nomaninnovates <nomansandhu0125@gmail.com>
2024-03-27 00:59:34 +0500

    Added dasboard route for charts

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,20 @@ import NoPage from "./pages/NoPage";
 import CreateUser from "./pages/CreateUser";
 import { UserContext } from "./context";
 import { useState } from "react";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [currentUser, setCurrentUser] = useState({UID:"test"});
   return (
     <>
+    
       <UserContext.Provider value={{ currentUser, setCurrentUser }}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/users" element={<Users />} />
             <Route path="/create-user" element={<CreateUser />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="*" element={<NoPage />} />
           </Routes>
         </BrowserRouter>
---
+++ src/components/charts/AreaChart.tsx
@@ -0,0 +1,56 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { getRandomNumber } from "../../utils";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: "Dataset 2",
+      data: labels.map(() => getRandomNumber(0, 1000)),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export default function AreaChart() {
+  return <Line options={options} data={data} />;
+}
